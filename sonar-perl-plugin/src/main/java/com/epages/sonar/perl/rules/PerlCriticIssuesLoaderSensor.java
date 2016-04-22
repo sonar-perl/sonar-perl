6a038b83b6623a1daed1e6f2ec81e414360794ab
Oliver Trosien <otrosien@epages.com>
2016-04-22 17:38:27 +0200

    hide test code violations (again)

--- sonar-perl-plugin/src/main/java/com/epages/sonar/perl/rules/PerlCriticIssuesLoaderSensor.java
+++ sonar-perl-plugin/src/main/java/com/epages/sonar/perl/rules/PerlCriticIssuesLoaderSensor.java
@@ -87,7 +87,8 @@ public class PerlCriticIssuesLoaderSensor implements Sensor {
         log.debug(violation.toString());
 
         InputFile inputFile = fileSystem
-                .inputFile(fileSystem.predicates().hasRelativePath(violation.getFilePath()));
+                .inputFile(fileSystem.predicates().and(fileSystem.predicates().hasRelativePath(violation.getFilePath()),
+                        fileSystem.predicates().hasType(InputFile.Type.MAIN)));
 
         if (inputFile != null) {
             saveIssue(inputFile, violation.getLine(), violation.getType(), violation.getDescription());
