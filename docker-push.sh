7e900319a3d30685e4f7f849bd370a42d53a4bfa
Oliver Trosien <otrosien@epages.com>
2016-11-25 14:17:55 +0100

    Tag docker images fix.

--- docker-push.sh
+++ docker-push.sh
@@ -2,14 +2,17 @@
 
 set -e # Exit with nonzero exit code if anything fails
 
+docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+export REPO=otrosien/sonar-perl
+
+if [[ "$TRAVIS_TAG" =~ ^[0-9.]+$ ]] ; then
+	docker tag sonar-perl $REPO:$TRAVIS_TAG
+	# push tag and latest
+	docker push $REPO:$TRAVIS_TAG
+	docker push $REPO
+fi
+
 if [ "$TRAVIS_BRANCH" == "master" ]; then
-	docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
-	export REPO=otrosien/sonar-perl
-	if [[ "$TRAVIS_TAG" =~ ^[0-9.]+$ ]] ; then
-		docker tag sonar-perl $REPO:$TRAVIS_TAG
-		# push tag
-		docker push $REPO:$TRAVIS_TAG
-	fi
 	# push latest
 	docker push $REPO
 fi
