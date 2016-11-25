77e74b085934f75f61e05260301ad07236624115
Oliver Trosien <otrosien@epages.com>
2016-11-25 14:08:48 +0100

    Tag docker images. Closes #17)

--- docker-push.sh
+++ docker-push.sh
@@ -5,5 +5,11 @@ set -e # Exit with nonzero exit code if anything fails
 if [ "$TRAVIS_BRANCH" == "master" ]; then
 	docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
 	export REPO=otrosien/sonar-perl
+	if [[ "$TRAVIS_TAG" =~ ^[0-9.]+$ ]] ; then
+		docker tag sonar-perl $REPO:$TRAVIS_TAG
+		# push tag
+		docker push $REPO:$TRAVIS_TAG
+	fi
+	# push latest
 	docker push $REPO
 fi
