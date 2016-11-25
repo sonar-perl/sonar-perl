0c5de1510c5616b903b07b89634cc67c0c6e8024
Oliver Trosien <otrosien@epages.com>
2016-11-25 13:43:25 +0100

    Try github publishing

--- docker-push.sh
+++ docker-push.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 set -e # Exit with nonzero exit code if anything fails
 
--- github-publish.sh
+++ github-publish.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
+#!/bin/bash -x
 
 set -e # Exit with nonzero exit code if anything fails
 
 if [[ "$TRAVIS_TAG" =~ ^[0-9.]+$ ]] ; then
-	tools/github-release release --security-token $GITHUB_TOKEN --user otrosien --repo sonar-perl --tag $TRAVIS_TAG --description "Release $TRAVIS_TAG"
+	tools/github-release upload --security-token "$GITHUB_TOKEN" --user otrosien --repo sonar-perl --tag "$TRAVIS_TAG" --name sonar-perl-plugin-${TRAVIS_TAG}.jar --file sonar-perl-plugin/build/libs/sonar-perl-plugin-${TRAVIS_TAG}.jar
+	tools/github-release release --security-token "$GITHUB_TOKEN" --user otrosien --repo sonar-perl --tag "$TRAVIS_TAG" --description "Release $TRAVIS_TAG"
 fi
