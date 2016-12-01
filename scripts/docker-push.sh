ce684c4983fe8cf0080701bb30c329876c7400bd
Oliver Trosien <otrosien@epages.com>
2016-12-01 08:10:49 +0100

    Trying to fix sonar reporting

--- scripts/docker-push.sh
+++ scripts/docker-push.sh
@@ -2,17 +2,23 @@
 
 set -e # Exit with nonzero exit code if anything fails
 
-docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+
+function docker_login {
+	docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+}
+
 export REPO=otrosien/sonar-perl
 
 if [[ "$TRAVIS_TAG" =~ ^[0-9.]+$ ]] ; then
+	docker_login
 	docker tag $REPO:latest $REPO:$TRAVIS_TAG
-	# push tag and latest
+	# push version tag and latest
 	docker push $REPO:$TRAVIS_TAG
 	docker push $REPO
 fi
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then
+	docker_login
 	# push latest
 	docker push $REPO
 fi
