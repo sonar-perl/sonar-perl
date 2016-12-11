d34dd1fb9748c00914868755dd78b939dc8d5c85
Oliver Trosien <otrosien@epages.com>
2016-12-11 13:12:31 +0100

    Improve cov. for PerlCritic

--- sonar-perl-plugin/src/main/java/com/github/otrosien/sonar/perl/critic/PerlCritic.java
+++ sonar-perl-plugin/src/main/java/com/github/otrosien/sonar/perl/critic/PerlCritic.java
@@ -1,13 +1,9 @@
 package com.github.otrosien.sonar.perl.critic;
 
-public class PerlCritic {
+public class PerlCritic { // NOSONAR - yes, this is a util class.
 
     public static final String PERLCRITIC_REPORT_PATH_KEY = "sonar.perlcritic.reportPath";
 
     public static final String PERLCRITIC_REPORT_PATH_DEFAULT = "perlcritic_report.txt";
-
-    private PerlCritic() {
-        throw new UnsupportedOperationException("Do not instanciate.");
-    }
 
 }
