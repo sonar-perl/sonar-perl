1a19ed81ee75508403a2dabd640cd6fdd9343b33
Oliver Trosien <otrosien@epages.com>
2016-12-11 13:10:59 +0100

    Unit test for PerlPlugin

---
+++ sonar-perl-plugin/src/test/java/com/github/otrosien/sonar/perl/PerlPluginTest.java
@@ -0,0 +1,19 @@
+package com.github.otrosien.sonar.perl;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.sonar.api.Plugin;
+import org.sonar.api.utils.Version;
+
+public class PerlPluginTest {
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void test() {
+        Plugin.Context context = new Plugin.Context(Version.create(5, 6));
+        new PerlPlugin().define(context);
+        assertThat(context.getExtensions()).hasSize(6);
+    }
+
+}
