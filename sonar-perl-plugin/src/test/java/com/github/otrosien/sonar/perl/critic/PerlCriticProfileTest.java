6cf5ca6b4581e9788ca19a26f2c64e8e26d4ccac
Oliver Trosien <otrosien@epages.com>
2016-12-11 13:25:54 +0100

    Unit test for PerlCriticProfile

---
+++ sonar-perl-plugin/src/test/java/com/github/otrosien/sonar/perl/critic/PerlCriticProfileTest.java
@@ -0,0 +1,52 @@
+package com.github.otrosien.sonar.perl.critic;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
+import org.sonar.api.profiles.RulesProfile;
+import org.sonar.api.profiles.XMLProfileParser;
+import org.sonar.api.rules.Rule;
+import org.sonar.api.rules.RuleFinder;
+import org.sonar.api.utils.ValidationMessages;
+
+import com.github.otrosien.sonar.perl.PerlLanguage;
+
+@RunWith(MockitoJUnitRunner.class)
+@SuppressWarnings("deprecation")
+public class PerlCriticProfileTest {
+
+    @Mock
+    RuleFinder ruleFinder;
+
+    @Test
+    public void should_create_perlcritic_profile() {
+      ValidationMessages validation = ValidationMessages.create();
+
+      PerlCriticProfile definition = new PerlCriticProfile(new XMLProfileParser(ruleFinder()));
+      RulesProfile profile = definition.createProfile(validation);
+      assertThat(profile.getLanguage()).isEqualTo(PerlLanguage.KEY);
+      assertThat(profile.getName()).isEqualTo("Sonar way");
+      assertThat(profile.getActiveRules()).extracting("repositoryKey").containsOnly("PerlCritic", "ePages");
+      assertThat(validation.hasErrors()).isFalse();
+      assertThat(profile.getActiveRules().size()).isGreaterThan(87);
+
+    }
+
+    RuleFinder ruleFinder() {
+      return when(ruleFinder.findByKey(anyString(), anyString())).thenAnswer(new Answer<Rule>() {
+        @Override
+        public Rule answer(InvocationOnMock invocation) {
+          Object[] arguments = invocation.getArguments();
+          return Rule.create((String) arguments[0], (String) arguments[1], (String) arguments[1]);
+        }
+      }).getMock();
+    }
+
+}
