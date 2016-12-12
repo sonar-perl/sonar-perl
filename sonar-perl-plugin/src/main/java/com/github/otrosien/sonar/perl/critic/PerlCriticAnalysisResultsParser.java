be1e7deb340feb718760f29f830d83403c441e39
Oliver Trosien <otrosien@epages.com>
2016-12-12 08:14:10 +0100

    Added some Javadoc

--- sonar-perl-plugin/src/main/java/com/github/otrosien/sonar/perl/critic/PerlCriticAnalysisResultsParser.java
+++ sonar-perl-plugin/src/main/java/com/github/otrosien/sonar/perl/critic/PerlCriticAnalysisResultsParser.java
@@ -18,7 +18,6 @@ class PerlCriticAnalysisResultsParser {
     public List<PerlCriticViolation> parse(final File file) throws IOException {
         PerlCriticIssuesLoaderSensor.log.info("Parsing file {}", file.getAbsolutePath());
 
-        // TODO: support run-away lines sometimes produced by perlcritc.
         try (Stream<String> lines = Files.lines(file.toPath())) {
             return lines //
                     .map(this::parseLine) //
@@ -28,7 +27,21 @@ class PerlCriticAnalysisResultsParser {
         }
     }
 
-    // see https://github.com/jploski/epic-ide/blob/b70f1c5ccb3e528f3a8c727b04dc633439f1d35a/org.epic.perleditor/src/org/epic/perleditor/editors/util/SourceCritic.java
+    /**
+     * Parse Perl::Critic report file. Format inspired by EPIC
+     * 
+     *  // 0 = file
+     *  // 1 = severity
+     *  // 2 = line
+     *  // 3 = column
+     *  // 4 = message
+     *  // 5 = PBP
+     *  // 6 = policy
+     *
+     *  // %f~|~%s~|~%l~|~%c~|~%m~|~%e~|~%p~||~%n
+     *
+     * See {@link https://github.com/jploski/epic-ide/blob/b70f1c5ccb3e528f3a8c727b04dc633439f1d35a/org.epic.perleditor/src/org/epic/perleditor/editors/util/SourceCritic.java}
+     */
     private Optional<PerlCriticViolation> parseLine(String line) {
         if(line.endsWith("OK")) {
             return Optional.empty();
@@ -37,17 +50,17 @@ class PerlCriticAnalysisResultsParser {
             log.warn("Invalid line '{}'", line);
             return Optional.empty();
         }
-        String[] tmp = line.replace("~||~", "").split("~\\|~");
+        String[] fields = line.replace("~||~", "").split("~\\|~");
 
-        // handle cases where a line returned from critic doesn't have all 7
-        // expected fields
-        if (tmp.length != 7) {
+        // TODO handle cases where a line returned from critic doesn't have all 7
+        // expected fields, e.g. when dealing with messages containing newlines.
+        if (fields.length != 7) {
             log.warn("Invalid line. Did not find 7 fields delimited by '~|~' in '{}'", line);
             return Optional.empty();
         }
 
         // @see org.epic.perleditor.editors.util.SourceCritic
-        return Optional.of(new PerlCriticViolation(tmp[6], tmp[4], tmp[0], "".equals(tmp[2]) ? -1 : Integer.parseInt(tmp[2])));
+        return Optional.of(new PerlCriticViolation(fields[6], fields[4], fields[0], "".equals(fields[2]) ? -1 : Integer.parseInt(fields[2])));
     }
 
 }
