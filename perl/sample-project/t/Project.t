65b56ba382ee582112adab0270043e624bbd7871
Oliver Trosien <otrosien@epages.com>
2016-12-15 08:50:58 +0100

    Simplicistic TAP parsing

--- perl/sample-project/t/Project.t
+++ perl/sample-project/t/Project.t
@@ -4,6 +4,7 @@ use Sample::Project;
 
 my $project = Sample::Project->new();
 is($project->hello, 'hello', 'should be hello');
+fail("Have some failing test");
 done_testing;
 
 1;
--- sonar-perl-plugin/src/main/java/com/github/otrosien/sonar/perl/tap/TestHarnessArchiveReader.java
+++ sonar-perl-plugin/src/main/java/com/github/otrosien/sonar/perl/tap/TestHarnessArchiveReader.java
@@ -10,6 +10,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
 import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
@@ -17,44 +19,78 @@ import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream;
 import org.sonar.api.utils.log.Logger;
 import org.sonar.api.utils.log.Loggers;
 
+import com.esotericsoftware.yamlbeans.YamlException;
 import com.esotericsoftware.yamlbeans.YamlReader;
 import com.github.otrosien.sonar.perl.tap.TestHarnessReport.Test;
+import com.github.otrosien.sonar.perl.tap.TestHarnessReport.TestDetail;
+import com.github.otrosien.sonar.perl.tap.TestHarnessReport.TestDetail.TestDetailBuilder;
+import com.github.otrosien.sonar.perl.tap.TestHarnessReport.TestHarnessReportBuilder;
 
 public class TestHarnessArchiveReader {
 
     private static final Logger log = Loggers.get(TestHarnessArchiveReader.class);
 
-    @SuppressWarnings("unchecked")
+    private Pattern tapNumberOfTests = Pattern.compile("^1\\.\\.(\\d+).*");
+
     public Optional<TestHarnessReport> read(File file) throws IOException {
 
+        TestHarnessReport.TestHarnessReportBuilder builder = TestHarnessReport.builder();
         try (FileInputStream s = new FileInputStream(file)) {
             TarArchiveInputStream tarInput = new TarArchiveInputStream(new GzipCompressorInputStream(s));
             TarArchiveEntry currentEntry = tarInput.getNextTarEntry();
-            TestHarnessReport report = null;
             while (currentEntry != null) {
                 if ("meta.yml".equals(currentEntry.getName())) {
-                    TestHarnessReport.TestHarnessReportBuilder builder = TestHarnessReport.builder();
                     log.info("Reading file entry {} from archive.", currentEntry.getName());
-                    BufferedReader br =  new BufferedReader(new InputStreamReader(tarInput));
-                    YamlReader reader = new YamlReader(br);
-                    Map<String, Object> object = (Map<String, Object>) reader.read(Map.class);
-                    builder.startTime(getFromMap(object, "start_time"));
-                    builder.endTime(getFromMap(object, "stop_time"));
-                    for (Map<String, Object> fileAttr : (List<Map<String, Object>>) object.get("file_attributes")) {
-                        builder.addTest(new Test((String) fileAttr.get("description"),
-                                new BigDecimal((String) fileAttr.get("start_time")),
-                                new BigDecimal((String) fileAttr.get("end_time"))));
-                    }
-                    report = builder.build();
-                    break;
+                    readMetaYaml(builder, tarInput);
+                } else {
+                    readTap(builder, tarInput);
                 }
                 currentEntry = tarInput.getNextTarEntry();
             }
-            return Optional.ofNullable(report);
+            return Optional.of(builder.build());
         } catch (NumberFormatException e) {
             log.error("Unable to parse report.", e);
         }
         return Optional.empty();
+    }
+
+    private void readTap(TestHarnessReportBuilder builder, TarArchiveInputStream tarInput) {
+        BufferedReader br =  new BufferedReader(new InputStreamReader(tarInput));
+        TestDetailBuilder detailBuilder = TestDetail.builder();
+        detailBuilder.filePath(tarInput.getCurrentEntry().getName());
+        br.lines().forEach(line -> {
+            if (line.startsWith("ok ")) {
+                detailBuilder.ok();
+            } else if (line.startsWith("not ok ")) {
+                detailBuilder.failed();
+            } else if (line.startsWith("1..")) {
+                Matcher m = tapNumberOfTests.matcher(line);
+                if (m.matches()) {
+                    detailBuilder.total(Integer.valueOf(m.group(1)));
+                }
+            }
+        });
+        TestDetail detail = detailBuilder.build();
+        if(detail.getNumberOfTests() > 0) {
+            builder.addTestDetail(detail);
+        } else {
+            log.info("Did not recognize TAP or test skipped completely: " + detail.getFilePath());
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private void readMetaYaml(TestHarnessReport.TestHarnessReportBuilder builder, TarArchiveInputStream tarInput)
+            throws YamlException {
+        BufferedReader br =  new BufferedReader(new InputStreamReader(tarInput));
+        YamlReader reader = new YamlReader(br);
+        Map<String, Object> object = (Map<String, Object>) reader.read(Map.class);
+        builder.startTime(getFromMap(object, "start_time"));
+        builder.endTime(getFromMap(object, "stop_time"));
+        for (Map<String, Object> fileAttr : (List<Map<String, Object>>) object.get("file_attributes")) {
+            builder.addTest(new Test((String) fileAttr.get("description"),
+                    new BigDecimal((String) fileAttr.get("start_time")),
+                    new BigDecimal((String) fileAttr.get("end_time"))));
+        }
     }
 
     private BigDecimal getFromMap(Map<String, Object> map, String key) {
