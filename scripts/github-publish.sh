58da111d4b159b9ab0e5eceea68fc4d9d3af045b
Oliver Trosien <oli@pocket-design.de>
2017-05-26 22:02:24 +0200

    fix github publishing

--- scripts/github-publish.sh
+++ scripts/github-publish.sh
@@ -3,6 +3,6 @@
 set -e # Exit with nonzero exit code if anything fails
 
 if [[ "$TRAVIS_TAG" =~ ^[0-9.]+$ ]] ; then
-	tools/github-release release --security-token "$GITHUB_TOKEN" --user otrosien --repo sonar-perl --tag "$TRAVIS_TAG" --description "Release $TRAVIS_TAG"
-	tools/github-release upload --security-token "$GITHUB_TOKEN" --user otrosien --repo sonar-perl --tag "$TRAVIS_TAG" --name sonar-perl-plugin-${TRAVIS_TAG}.jar --file sonar-perl-plugin/build/libs/sonar-perl-plugin-${TRAVIS_TAG}.jar
+	tools/github-release release --security-token "$GITHUB_TOKEN" --user sonar-perl --repo sonar-perl --tag "$TRAVIS_TAG" --description "Release $TRAVIS_TAG"
+	tools/github-release upload --security-token "$GITHUB_TOKEN" --user sonar-perl --repo sonar-perl --tag "$TRAVIS_TAG" --name sonar-perl-plugin-${TRAVIS_TAG}.jar --file sonar-perl-plugin/build/libs/sonar-perl-plugin-${TRAVIS_TAG}.jar
 fi
