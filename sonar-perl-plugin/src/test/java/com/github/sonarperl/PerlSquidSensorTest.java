656378de3ad51a28d36859fc3248c16f334b6679
Oliver Trosien <oli@pocket-design.de>
2019-01-05 11:45:39 +0100

    test for perlsquidsensor

---
+++ sonar-perl-plugin/src/test/java/com/github/sonarperl/PerlSquidSensorTest.java
@@ -0,0 +1,88 @@
+package com.github.sonarperl;
+
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.sonar.api.batch.fs.InputFile;
+import org.sonar.api.batch.fs.InputFile.Type;
+import org.sonar.api.batch.fs.internal.DefaultInputFile;
+import org.sonar.api.batch.fs.internal.TestInputFileBuilder;
+import org.sonar.api.batch.rule.ActiveRules;
+import org.sonar.api.batch.rule.CheckFactory;
+import org.sonar.api.batch.rule.internal.ActiveRulesBuilder;
+import org.sonar.api.batch.sensor.internal.DefaultSensorDescriptor;
+import org.sonar.api.batch.sensor.internal.SensorContextTester;
+import org.sonar.api.issue.NoSonarFilter;
+import org.sonar.api.measures.FileLinesContext;
+import org.sonar.api.measures.FileLinesContextFactory;
+import org.sonar.api.utils.log.LogTester;
+
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class PerlSquidSensorTest {
+
+    private final File baseDir = new File("src/test/resources/com/github/sonarperl/plugins/perl/squid-sensor").getAbsoluteFile();
+
+    private SensorContextTester context;
+
+    private ActiveRules activeRules;
+
+    @org.junit.Rule
+    public LogTester logTester = new LogTester();
+
+    @Before
+    public void init() {
+        context = SensorContextTester.create(baseDir);
+    }
+
+    @Test
+    public void sensor_descriptor() {
+        activeRules = (new ActiveRulesBuilder()).build();
+        DefaultSensorDescriptor descriptor = new DefaultSensorDescriptor();
+        sensor().describe(descriptor);
+
+        assertThat(descriptor.name()).isEqualTo("Perl Squid Sensor");
+        assertThat(descriptor.languages()).containsOnly("perl");
+        assertThat(descriptor.type()).isEqualTo(Type.MAIN);
+    }
+
+    @Test
+    public void test_execute() {
+        inputFile("file1.pl");
+
+        sensor().execute(context);
+
+        String key = "moduleKey:file1.pl";
+        String msg = "number of TypeOfText for the highlighting of keyword 'sub'";
+        assertThat(context.highlightingTypeAt(key, 9, 2)).as(msg).hasSize(1);
+        assertThat(context.allAnalysisErrors()).isEmpty();
+    }
+
+    private PerlSquidSensor sensor() {
+        FileLinesContextFactory fileLinesContextFactory = mock(FileLinesContextFactory.class);
+        FileLinesContext fileLinesContext = mock(FileLinesContext.class);
+        when(fileLinesContextFactory.createFor(Mockito.any(InputFile.class))).thenReturn(fileLinesContext);
+        CheckFactory checkFactory = new CheckFactory(activeRules);
+        return new PerlSquidSensor(fileLinesContextFactory, checkFactory, new NoSonarFilter());
+    }
+
+    private InputFile inputFile(String name) {
+        DefaultInputFile inputFile =  TestInputFileBuilder.create("moduleKey", name)
+                .setModuleBaseDir(baseDir.toPath())
+                .setCharset(StandardCharsets.UTF_8)
+                .setType(Type.MAIN)
+                .setLanguage(PerlLanguage.KEY)
+                .initMetadata(TestUtils.fileContent(new File(baseDir, name), StandardCharsets.UTF_8))
+                .build();
+        context.fileSystem().add(inputFile);
+        return inputFile;
+    }
+
+}
+
---
+++ sonar-perl-plugin/src/test/resources/com/github/sonarperl/plugins/perl/squid-sensor/file1.pl
@@ -0,0 +1,11 @@
+# Integers
+00000;
+# strings
+"lala";
+print; # the grammar used should recognize this as builtin
+$a = [1,
+     2, # after comment
+     ];
+sub foo(){}
+package bar;
+sub foo2($x,$y,$z);
