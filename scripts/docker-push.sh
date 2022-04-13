266662f79501c9dc0a8581ab50012565c187e1cc
Oliver Trosien <otrosien@users.noreply.github.com>
2022-04-13 08:50:18 +0200

    docker push (#137)

--- scripts/docker-push.sh
+++ scripts/docker-push.sh
@@ -2,22 +2,23 @@
 
 set -e # Exit with nonzero exit code if anything fails
 
-
 function docker_login {
 	docker login -u $DOCKER_USER -p $DOCKER_PASS
 }
 
 export REPO=sonarperl/sonar-perl
 
-if [[ "$TRAVIS_TAG" =~ ^[0-9.]+$ ]] ; then
+RAW_REF=${GITHUB_REF##*/}
+
+if [[ "$GITHUB_REF" =~ ^refs/tags/[0-9.]+$ ]] ; then
 	docker_login
-	docker tag $REPO:latest $REPO:$TRAVIS_TAG
+	docker tag $REPO:latest $REPO:$RAW_REF
 	# push version tag and latest
-	docker push $REPO:$TRAVIS_TAG
+	docker push $REPO:$RAW_REF
 	docker push $REPO
 fi
 
-if [ "$TRAVIS_BRANCH" == "master" ]; then
+if [ "$GITHUB_REF" == "refs/heads/master" ]; then
 	docker_login
 	# push latest
 	docker push $REPO
