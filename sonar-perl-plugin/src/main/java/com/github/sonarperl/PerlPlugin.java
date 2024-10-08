6ba65848970dbd873ecb4a5080e7b09b941ff5ae
Ron Behlmann <rdbehlmann@gmail.com>
2024-10-08 16:02:18 -0400

    Property style changes from SonarQube 10.4 (#1)
    * Property style changes from SonarQube 10.4
    
    * Update PerlLanguageTest.java to reflect change in FILE_SUFFIXES_KEY property location.
    
    * Update PerlSquidSensorTest.java to reflect change in LogTester class originating from v9.15 change in sonar-plugin-api
    
    * Update build.gradle to reference 9.15 test-fixtures artifact
    
    * Update PerlLanguageTest.java to reflect change in defaults
    
    * Update PerlLanguageTest.java
    
    * Update PerlLanguageTest.java to create lists in JAVA!
    
    * Update PerlLanguageTest.java due to the downfalls of using a browser-based code editor...
    
    * Update PerlLanguageTest.java mapsettings.setproperty does not support list/array.
    
    * Typos everywhere...
    
    * IntegrationTests version updates and artifactory_url override to mavenCentral
    
    * Updates to tests and associated logic to reflect changes from v10.4 changes
    
    * Update ci.yml to set the repo based off the GITHUB_REPOSITORY variable vice hardcoded
    
    * Changes from sonar findings and env variable added to core build.gradle to run outside of parent project
    
    * Retriggering PR build
    
    * Update ci.yml to set env variable
    
    * Update ci.yml misread page
    
    * Update ci.yml stupid typo...
    
    * Update ci.yml more fun.
    
    * Update ci.yml adding branch name ref
    
    * Update build.gradle adding branch name ref for sonarscan
    
    * Update ci.yml updating action version and adding gradle setup
    
    * Correcting sonar branch name property reference
    
    * Update build.gradle testing to see if /ref issue is due to mapping
    
    * Update build.gradle
    
    * Remediate Sonar findings

--- sonar-perl-plugin/src/main/java/com/github/sonarperl/PerlPlugin.java
+++ sonar-perl-plugin/src/main/java/com/github/sonarperl/PerlPlugin.java
@@ -1,12 +1,9 @@
 package com.github.sonarperl;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import org.sonar.api.Plugin;
-import org.sonar.api.Properties;
-import org.sonar.api.Property;
+import org.sonar.api.PropertyType;
+import org.sonar.api.config.PropertyDefinition;
+import org.sonar.api.resources.Qualifiers;
 
 import com.github.sonarperl.critic.PerlCriticIssuesLoaderSensor;
 import com.github.sonarperl.critic.PerlCriticProperties;
@@ -15,34 +12,11 @@ import com.github.sonarperl.critic.SonarWayProfile;
 import com.github.sonarperl.tap.TestHarnessArchiveProperties;
 import com.github.sonarperl.tap.TestHarnessLoaderSensor;
 
-@Properties({ //
-    @Property( //
-    key = PerlPlugin.FILE_SUFFIXES_KEY, //
-    name = "File Suffixes", //
-    description = "Comma-separated list of suffixes for files to analyze.", //
-    defaultValue = PerlPlugin.DEFAULT_FILE_SUFFIXES,
-    multiValues = true), //
-    @Property( //
-    key = PerlCriticProperties.PERLCRITIC_REPORT_PATH_KEY, //
-    name = "PerlCritic Report Location", //
-    description = "Location of perlcritic report file. Can be generated using this command-line: perlcritic --quiet --verbose \"%f~|~%s~|~%l~|~%c~|~%m~|~%e~|~%p~||~%n\"", //
-    defaultValue = PerlCriticProperties.PERLCRITIC_REPORT_PATH_DEFAULT ), //
-    @Property( //
-    key = TestHarnessArchiveProperties.HARNESS_ARCHIVE_PATH_KEY, //
-    name = "Test::Harness Archive Location", //
-    description = "Location of Test::Harness::Archive report file. Can be generated using this command-line: prove -t -a testReport.tgz", //
-    defaultValue = TestHarnessArchiveProperties.HARNESS_ARCHIVE_PATH_DEFAULT ) //
-})
 public class PerlPlugin implements Plugin {
+    public static final String CATEGORY_NAME = "Community Perl";
 
-    public static final String FILE_SUFFIXES_KEY = "com.github.sonarperl.suffixes";
-
-    public static final String DEFAULT_FILE_SUFFIXES = "pm,pl,t";
-
-    @Override
     public void define(Context context) {
-        List<Object> extensions = new ArrayList<>();
-        extensions.addAll(Arrays.asList(
+        context.addExtensions(
                 PerlLanguage.class,
                 PerlSquidSensor.class,
                 PerlCriticRulesDefinition.class, 
@@ -50,9 +24,37 @@ public class PerlPlugin implements Plugin {
                 GlobalSensor.class,
                 PerlCriticIssuesLoaderSensor.class,
                 TestHarnessLoaderSensor.class
-        ));
+        );
 
-        context.addExtensions(extensions);
+        context.addExtension(PropertyDefinition.builder(PerlLanguage.FILE_SUFFIXES_KEY)
+                .name("File Suffixes")
+                .description("Comma-separated list of suffixes for files to analyze.")
+                .defaultValue(String.join(",", PerlLanguage.FILE_SUFFIXES))
+                .category(CATEGORY_NAME)
+                .subCategory("General")
+                .onQualifiers(Qualifiers.PROJECT)
+                .type(PropertyType.STRING)
+                .multiValues(true)
+                .build());
+        context.addExtension(PropertyDefinition.builder(PerlCriticProperties.PERLCRITIC_REPORT_PATH_KEY)
+                .name("PerlCritic Report Location")
+                .description("Location of perlcritic report file. Can be generated using this command-line: perlcritic --quiet --verbose \"%f~|~%s~|~%l~|~%c~|~%m~|~%e~|~%p~||~%n\"")
+                .defaultValue(PerlCriticProperties.PERLCRITIC_REPORT_PATH_DEFAULT)
+                .category(CATEGORY_NAME)
+                .subCategory("Perl::Critic")
+                .onQualifiers(Qualifiers.PROJECT)
+                .type(PropertyType.STRING)
+                .build());
+        context.addExtension(PropertyDefinition.builder(TestHarnessArchiveProperties.HARNESS_ARCHIVE_PATH_KEY)
+                .name("Test::Harness Archive Location")
+                .description("Location of Test::Harness::Archive report file. Can be generated using this command-line: prove -t -a testReport.tgz")
+                .defaultValue(TestHarnessArchiveProperties.HARNESS_ARCHIVE_PATH_DEFAULT)
+                .category(CATEGORY_NAME)
+                .subCategory("Test::Harness")
+                .onQualifiers(Qualifiers.PROJECT)
+                .type(PropertyType.STRING)
+                .build());
+        
     }
 
 }
